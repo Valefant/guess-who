590b62974a0b4e2541d61abce8f35b8a26cf4e3e
Valefant <19901781+Valefant@users.noreply.github.com>
2023-08-27 11:49:19 +0200

    Check jq dependency in script

--- sort-translations.sh
+++ sort-translations.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if [ -z $(which jq 2>/dev/null) ]; then
+  echo 'jq is required'
+  exit 1
+fi
+
 languages="en-US de"
 for language in $languages; do
   file_path="src/i18n/$language.json"
